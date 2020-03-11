95a970348c227a90c85bc7ce305e63521c2f0a96
guigui0159570 <62026254+guigui0159570@users.noreply.github.com>
2020-03-11 18:59:48 +0100

    Accueil

---
+++ script.js
@@ -0,0 +1,43 @@
+function accueil() {
+	window.location.href=('Accueil.html');
+}
+
+function sauverlemonde() {
+	window.location.href=('Sauver le monde.html');
+}
+
+function battleroyale() {
+	window.location.href=('Battle Royale.html');
+}
+
+function modecreatif() {
+	window.location.href=('MODE CRÉATIF.html');
+}
+
+function proam() {
+	window.location.href=('PRO AM.html')
+}
+
+function coupedumonde() {
+	window.location.href=('Coupe du monde.html');
+}
+
+function chapitre2() {
+	window.location.href=('chapitre 2.html');
+}
+
+function cmsolo() {
+	window.location.href=('Coupe du monde solo.html');
+}
+
+function cmduo() {
+	window.location.href=('Coupe du monde duo.html');
+}
+
+function cmcrea() {
+	window.location.href=('Coupe du monde mode créa.html')
+}
+
+function influance() {
+	window.location.href=('influance.html')
+}
